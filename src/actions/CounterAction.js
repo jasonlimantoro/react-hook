16498c211e6dd74291846dbf24724db21f7a9ab8
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-11-13 14:59:21 +0800

    extract actions

---
+++ src/actions/CounterAction.js
@@ -0,0 +1,23 @@
+export const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+};
+
+export const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+};
+
+export const quadratic = () => {
+  return {
+    type: "QUADRATIC"
+  }
+};
+
+export const sqrt = () => {
+  return {
+    type: "SQUARE ROOT"
+  }
+};
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,33 @@
 import React, { useEffect, } from 'react';
 import useCounterReducer from "../reducers/CounterReducer";
+import * as CounterActionCreators from "../actions/CounterAction";
 
 
 const Counter = ({ initial, autoIncrement, autoDecrement }) => {
   const { state, dispatch } = useCounterReducer(initial);
   useAutoCounter(autoIncrement, autoDecrement, dispatch);
 
+  const boundIncrement = () => (
+    dispatch(CounterActionCreators.increment())
+  );
+  const boundDecrement = () => (
+    dispatch(CounterActionCreators.decrement())
+  );
+  const boundQuadratic = () => (
+    dispatch(CounterActionCreators.quadratic())
+  );
+  const boundSqrt = () => (
+    dispatch(CounterActionCreators.sqrt())
+  );
+
   return (
     <div>
       <span>{state.counter}</span>
       <div>
-        <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
-        <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
-        <button onClick={() => dispatch({ type: 'QUADRATIC' })}>^</button>
-        <button onClick={() => dispatch({ type: 'SQUARE ROOT' })}>&#8730;</button>
+        <button onClick={boundIncrement}>+</button>
+        <button onClick={boundDecrement}>-</button>
+        <button onClick={boundQuadratic}>^</button>
+        <button onClick={boundSqrt}>&#8730;</button>
       </div>
     </div>
   );
